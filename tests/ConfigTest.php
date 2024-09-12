bfa71887ddf884f4a801750dfd1cda3c5beeb1b4
omegacms <omegacms@outlook.com>
2024-09-12 17:44:28 +0200

    Add Test

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -22,17 +22,19 @@ namespace Omega\Config\Tests;
 /**
  * @use
  */
-use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\TestDox;
 use Omega\Config\Config;
+use Omega\Testing\TestCase;
 use ReflectionClass;
 use ReflectionMethod;
 
 /**
  * ConfigTest class.
  *
- * The `ConfigTest` class contains tests for the `Config` class, including  verifying 
- * the retrieval of configuration values using dot notation, handling default values, 
- * and accessing private methods through  reflection. It also  ensures that temporary 
+ * The `ConfigTest` class contains tests for the `Config` class, including  verifying
+ * the retrieval of configuration values using dot notation, handling default values,
+ * and accessing private methods through  reflection. It also  ensures that temporary
  * configuration files are created and managed correctly during testing.
  *
  * @category    Omega
@@ -47,7 +49,7 @@ class ConfigTest extends TestCase
 {
     /**
      * Config instance.
-     * 
+     *
      * @return Config $config Holds the config instance.
      */
     private Config $config;
@@ -75,7 +77,13 @@ class ConfigTest extends TestCase
      *
      * @return void
      */
-    public function testGetConfigValueWithDotNotation(): void
+    #[Test]
+    #[TestDox('Tests retrieval of configuration values using dot notation.
+
+    This test verifies that the `Config` class correctly retrieves nested configuration
+    values specified using dot notation.
+    ')]
+    public function getConfigValueWithDotNotation(): void
     {
         $mockConfig = [
             'database' => [
@@ -92,13 +100,11 @@ class ConfigTest extends TestCase
             ]
         ];
 
-        // Usa la riflessione per mockare il metodo loadConfigFile
         $reflection = new ReflectionClass($this->config);
         $method = $reflection->getMethod('loadConfigFile');
         $method->setAccessible(true);
         $method->invoke($this->config, $this->createTempConfigFile($mockConfig));
 
-        // Usa la riflessione per mockare il metodo withDots
         $method = $reflection->getMethod('withDots');
         $method->setAccessible(true);
 
@@ -114,7 +120,13 @@ class ConfigTest extends TestCase
      *
      * @return void
      */
-    public function testGetConfigValueWithDefault(): void
+    #[Test]
+    #[TestDox('Tests retrieval of configuration values with a default value.
+
+    This test checks if the `Config` class returns the default value when the requested
+    configuration key is not found.
+    ')]
+    public function getConfigValueWithDefault(): void
     {
         $mockConfig = [
             'database' => [
@@ -153,7 +165,13 @@ class ConfigTest extends TestCase
      *
      * @return void
      */
-    public function testPrivateMethodWithDots(): void
+    #[Test]
+    #[TestDox('Tests the private method `withDots` using reflection.
+    
+    This test ensures that the `withDots` method, which is used to retrieve nested configuration
+    values, works correctly. The method is accessed using reflection.
+    ')]
+    public function privateMethodWithDots(): void
     {
         $mockConfig = [
             'database' => [
@@ -182,7 +200,13 @@ class ConfigTest extends TestCase
      *
      * @return void
      */
-    public function testPrivateMethodLoadConfigFile(): void
+    #[Test]
+    #[TestDox('Tests the private method `loadConfigFile` using reflection.
+    
+    This test verifies that the `loadConfigFile` method correctly loads configuration data from
+    a file. The method is accessed and tested using reflection.
+    ')]
+    public function privateMethodLoadConfigFile(): void
     {
         $mockConfig = [
             'database' => [
@@ -208,7 +232,6 @@ class ConfigTest extends TestCase
 
         $this->assertSame($mockConfig, $result);
 
-        // Pulisci il file temporaneo
         unlink($tempFile);
     }
 
@@ -218,10 +241,10 @@ class ConfigTest extends TestCase
      * This helper method creates a temporary file containing the provided configuration data,
      * which is used in the tests. The file is returned as a string representing its path.
      *
-     * @param array<string, mixed> $configData The configuration data to write to the file.
+     * @param  array<string, mixed> $configData The configuration data to write to the file.
      * @return string The path to the temporary configuration file.
      */
-    private function createTempConfigFile(array $configData): string
+    private function createTempConfigFile( array $configData ): string
     {
         $tempFile = sys_get_temp_dir() . '/test_config.php';
         file_put_contents($tempFile, '<?php return ' . var_export($configData, true) . ';');
