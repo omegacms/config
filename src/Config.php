0964d8895d8cfaabde1d0a04010ec4c18e175294
omegacms <omegacms@outlook.com>
2024-10-03 21:43:32 +0200

    Removed empty lines and updated the @category, @package, and @subpackage tags to current standards.

--- src/Config.php
+++ src/Config.php
@@ -32,7 +32,7 @@ use function file_exists;
  * parameters stored in PHP files within the `config` directory of your application.
  *
  * @category    Omega
- * @package     Omega\Config
+ * @package     Config
  * @link        https://omegacms.github.io
  * @author      Adriano Giovannini <omegacms@outlook.com>
  * @copyright   Copyright (c) 2024 Adriano Giovannini. (https://omegacms.github.io)
